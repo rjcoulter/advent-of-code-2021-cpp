81e4ce2ad5096b8591d0b3bc23803b0adae338c9
Ryan Coulter <ryancoulter22@gmail.com>
2021-12-17 10:56:31 -0500

    Complete day 1

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,13 +1,42 @@
 #include <fstream>
 #include <iostream>
+#include <vector>
 
-void puzzle1() {
-    std::ifstream file("input.txt");
-    std::string s;
-    while(std::getline(file, s)) {
-        std::cout << s << std::endl;
-    }
+void puzzle1(const std::vector<int>& measurements) {
+    int increases = 0;
+    for(int i = 1; i < measurements.size(); i++) {
+        if(measurements[i] > measurements[i - 1])
+            increases++;
+    } 
+    std::cout << "Number of increases: " << increases << std::endl;
 }
+
+void puzzle2(const std::vector<int>& measurements) {
+    int increases = 0;
+    std::vector<int> windows;
+    for(int i = 2; i < measurements.size(); i++) {
+        windows.push_back(measurements[i] + measurements[i - 1] + measurements[i - 2]);
+    }
+
+    for(int i = 1; i < windows.size(); i++) {
+        if(windows[i] > windows[i - 1])
+            increases++;
+    }
+    std::cout << "Number of increases: " << increases << std::endl;
+}
+
 int main() {
-    puzzle1();
+
+    // Open the file
+    std::ifstream file("input.txt");
+    std::vector<int> measurements;
+    while(file.eof() == false) {
+        int next;
+        file >> next;
+        measurements.push_back(next);
+    }
+    file.close();
+    
+    puzzle1(measurements);
+    puzzle2(measurements);
 }
